2065c9b753943ca07931c9849f87479c09b24795
Juan Picos <juanpicos98@outlook.es>
2022-09-05 23:41:26 -0500

    Se hicieron algunos ajustes

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ const UserSchema = new Schema ({
     password: {type:String, required:true}
 });
 
-const artistSchema = new Schema({
-    artist: {type: String, required: true, unique:true},
+const ArtistSchema = new Schema({
+    Artist: {type: String, required: true, unique:true},
 })
 
 const PaintsModel = model('paints', PaintSchema);
 const UserModel = model('users', UserSchema);
-const ArtistModel = model('artists', artistSchema);
+const ArtistModel = model('artists', ArtistSchema);
 
 
 const server = createServer(function (request, response){
@@ -46,6 +46,18 @@ const server = createServer(function (request, response){
                 response.end();
             } else {
                 response.write(JSON.stringify(paints));
+                response.end();
+            }
+        });
+    }
+    else if(request.url ==='/paints/artists'){
+        ArtistModel.find().populate('Artist').exec(function(err, artists){
+            if (err){
+                response.write('Request Failed');
+                console.error(err);
+                response.end();
+            } else {
+                response.write(JSON.stringify(artists));
                 response.end();
             }
         });
@@ -65,18 +77,18 @@ const server = createServer(function (request, response){
         });
     } else if (request.url.startsWith('/paints/artists/')){
         const split = request.url.split('/');
-        const artistId = split[3];
-        console.log(artistId);
-        ArtistModel.find({artist: Types.ObjectId(artistId)}, function(err,artists){
+        const ArtistId = split[3];
+        console.log(ArtistId);
+        ArtistModel.find({Artist: Types.ObjectId(ArtistId)}, function(err,Artist){
             if(err){
                 response.write('Failed to load paints');
                 console.error(err);
                 response.end();
             } else {
-                response.write(JSON.stringify(artists));
+                response.write(JSON.stringify(Artist));
                 response.end();
             }
-        })
+        });
     } else {
         response.write('Error 404: Page not found');
         response.end();
